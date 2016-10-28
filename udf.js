3d410979fc5770447fec36ef71e4b15426b799a4
tawat <twboonthai@gmail.com>
2016-10-28 15:29:56 +0700

    test add

--- udf.js
+++ udf.js
@@ -1,6 +1,6 @@
-
+﻿//Testing git clone
 /*
- * This file is provided for custom JavaScript logic that your HTML files might need.
+ * ABC DEF This file is provided for custom JavaScript logic that your HTML files might need.
  * Maqetta includes this JavaScript file by default within HTML pages authored in Maqetta.
  */
  
